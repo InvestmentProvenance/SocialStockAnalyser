9ff9b61b4fc4f549a9dc29871b5e759eb18433c3
ab2886 <ab2886>
2024-02-09 18:49:52 +0000

    Code works again

--- stock_api_caller.py
+++ stock_api_caller.py
@@ -2,19 +2,22 @@
 import os
 from datetime import datetime, timedelta
 import requests
+# import typing
 
 
 # Replace these API keys with your own keys from https://www.alphavantage.co/support/#api-key
-API_KEYS = ['YYLUN0RB2ER81GFR', 'U2AIMRQSAH58QCP2', '0IJGQ4OJUTZC7W6A', 'N2RS2KZ08K54CEQH',
-            'H19BQEDTB2YTBP3O']
+# API_KEYS = ['YYLUN0RB2ER81GFR', 'U2AIMRQSAH58QCP2', '0IJGQ4OJUTZC7W6A', 'N2RS2KZ08K54CEQH',
+#             'H19BQEDTB2YTBP3O']
+API_KEY = 'YYLUN0RB2ER81GFR'
 SYMBOL = 'GME'
-INTERVAL = '5min'
 
-def r(symbol, interval, key, month_str):
+def getStockData(symbol, month_str):
+    """Makes a Request to AlphaVantage API"""
+    interval = '5min'
     url = (f"https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY"
             f"&symbol={symbol}"
             f"&interval={interval}"
-            f"&apikey={key}"
+            f"&apikey={API_KEY}"
             f"&outputsize=full"
             f"&datatype=csv&month={month_str}")
     # Send the API request
@@ -28,41 +31,37 @@ def r(symbol, interval, key, month_str):
     else:
         return r.content
     
-def saveToFile(data):
-        # Save the data to a file on the desktop
-        with open(file_path, 'wb') as file:
-            file.write(r.content)
+def saveToFile(file_path, data):
+    # Save the data to a file on the desktop
+    with open(file_path, 'wb') as file:
+        file.write(data)
 
-        print(f"Data for {month_str} saved to {file_path}")
 
-        # Increment the API key index and reset the calls counter if necessary
-        calls_per_key += 1
-        if calls_per_key >= 25:
-            api_key_index = (api_key_index + 1) % len(API_KEYS)
-                    calls_per_key = 0
-
-API_KEYS[api_key_index]
-
-def downloadSymbolData(symbol, start_month=end_month - timedelta(days=60 * 30), end_month=datetime.now()):
-    """Makes a Request to AlphaVantage API and saves to a file"""
-    # Calculate the start and end months for the past 60 months
-    end_month = datetime.now()
-    start_month = end_month - timedelta(days=60 * 30)  # 30 days per month approximation
-
+def downloadSymbolData(symbol, end_month=datetime.now(), start_month=None):
+    MONTH_NUM = 60
+    DAYS_IN_MONTH = 30
+    if start_month == None: 
+        start_month = end_month - timedelta(days=MONTH_NUM * DAYS_IN_MONTH)
+    
     # Initialize counters for API keys and calls
-    api_key_index = 0
-    calls_per_key = 0
-
+    # api_key_index = 0
+    # calls_per_key = 0
     while start_month <= end_month:
-        # Format the date in YYYY-MM format
         month_str = start_month.strftime('%Y-%m')
-        desktop_path = os.path.expanduser("~/Desktop/Stockdata")
-        file_path = os.path.join(desktop_path, f"alphavantage_data_{SYMBOL}_{month_str}.csv")
+        desktop_path = os.path.expanduser("~")
+        file_path = os.path.join(desktop_path, f"alphavantage_data_{symbol}_{month_str}.csv")
         if os.path.exists(file_path):
             print(f"Data for {month_str} already saved.")
         else:
-            # Build the URL for the API request
-            
-
+            data = getStockData(symbol, month_str)
+            saveToFile(file_path, data)
+            print(f"Data for {start_month} saved to {file_path}")
         # Move to the previous month
         start_month = start_month + timedelta(days=30)
+        # Increment the API key index and reset the calls counter if necessary
+        # calls_per_key += 1
+        # if calls_per_key >= 25:
+        #     api_key_index = (api_key_index + 1) % len(API_KEYS)
+        #     calls_per_key = 0
+
+downloadSymbolData("SPCE")
