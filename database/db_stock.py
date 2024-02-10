aa6a007f8949cbe1aac93aceecf1aae5f63ab505
Reuben Carolan <reuben.carolan@gmail.com>
2024-02-10 19:15:16 +0000

    Tidy a few things up

--- database/db_stock.py
+++ database/db_stock.py
@@ -1,28 +1,29 @@
-import pymysql
+"""Provides wrapper for Database Operations"""
 from datetime import datetime, timedelta
+from typing import Callable
 import os
-import re
-import glob
+import pymysql
 
 
-#db = pymysql.connect(host = 'investdata.c5cwsai4kiot.us-east-1.rds.amazonaws.com',user = 'admin',password ='12345678')
 
 host = os.environ['DB_HOST']
 user = os.environ['DB_USER']
 password = os.environ['DB_PASSWORD']
-database = os.environ['DB_NAME']
+database_name = os.environ['DB_NAME']
 testing = os.environ['TESTING']
 
 #cursor = db.cursor()
 
-def DB_Operation(func):
-    def wrap(*args, **kwargs): 
+def db_operation(func: Callable[..., pymysql.connect ]):
+    """Decorator to Provide a database connection to the function with named argument 'database'
+      and close it after the operation is done. """
+    def wrap(*args, **kwargs):
         # Before each DB opertaion we have to establish a connection and then close it after
         try:
-            db = pymysql.connect(host=host, user=user, password=password, database=database)
+            db = pymysql.connect(host=host, user=user, password=password, database=database_name)
             print("trying operation")
             kwargs['database'] = db
-            return func(*args, **kwargs) 
+            return func(*args, **kwargs)
         except pymysql.Error as e:
             print(f"Error: {e}")
         finally:
@@ -30,11 +31,11 @@ def DB_Operation(func):
                 db.close()
                 print("Connection closed")
         return None
-        
-    return wrap 
+    return wrap
 
-@DB_Operation
-def upload_Stock(database:pymysql.connect, raw_data):
+@db_operation
+def upload_stock(database:pymysql.connect, raw_data):
+    """Uploads stock data to the database. raw_data is a collection of tuples"""
     cursor = database.cursor()
     insert_sql = """
     INSERT INTO stock_data (timestamp, open, high, low, close, volume, symbol)
@@ -42,17 +43,21 @@ def upload_Stock(database:pymysql.connect, raw_data):
     """
     cursor.executemany(insert_sql, raw_data)
     database.commit()
-    pass
 
-@DB_Operation
-def read_Stock(database:pymysql.connect, ticker:str,start_date:datetime,end_date:datetime):   #retrieves last 30 days chronologically ordered
+@db_operation
+def read_stock(
+    database:pymysql.connect,
+    ticker:str,
+    start_date:datetime,
+    end_date:datetime):   #retrieves last 30 days chronologically ordered
+    """Retrives the stock data for the given ticker from the database"""
     print("reading")
     start_date = end_date - timedelta(days=30)  # 30 days per month approximation
     cursor = database.cursor()
 
     # Define the SQL query to select all data from the table
-    select_query = "SELECT * FROM stock_data WHERE TimeStamp BETWEEN '{}' AND '{}' AND Symbol = '{}' ORDER BY TimeStamp".format(start_date,end_date,ticker)
-
+    select_query = (f"SELECT * FROM stock_data WHERE TimeStamp "
+    f"BETWEEN '{start_date}' AND '{end_date}' AND Symbol = '{ticker}' ORDER BY TimeStamp")
     print(select_query)
 
     # Execute the query
@@ -63,4 +68,3 @@ def read_Stock(database:pymysql.connect, ticker:str,start_date:datetime,end_date
 
     # Display the table data
     return table_data
-
