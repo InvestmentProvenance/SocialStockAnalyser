80069e9347debfe7ee7784dc0d1d46509d7f25d6
Reuben Carolan <reuben.carolan@gmail.com>
2024-02-10 20:34:49 +0000

    Tidied main.py

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 """Example file for testing docker container"""
-import os
-import database.DB_Stock as db_stock
-from database import Data as data
 import datetime
 from datetime import timedelta
+from database import data
+
 
 end_date = datetime.datetime(2022, 4,29)
 start_date = end_date - timedelta(days=30)  # 30 days per month approximation
 print("start date: ", start_date)
 print("end date: ", end_date)
-print(data.get_Data(ticker='GME',start_Time=start_date, end_Time=end_date))
+print(data.get_data(ticker='GME',start_time=start_date, end_time=end_date))
 
 #print(f"top secret database password: {os.environ['DATABASE_TOP_SECRET_KEY']}")
 print("Hello World!\n")
