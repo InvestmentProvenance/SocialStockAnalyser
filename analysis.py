d516be63ff4c8de710c95ec2099304b1e5ccbb74
ab2886 <ab2886>
2024-02-11 18:34:50 +0000

    x-axis is now datetime

--- analysis.py
+++ analysis.py
@@ -8,30 +8,27 @@ import matplotlib.dates as mdates
 COMMENTS_DATA = "wallstreetbets-posts-and-comments-for-august-2021-comments.csv"
 STOCK_DATA = ""
 
-def get_comment_table():
+def get_market_sentiment():
     """Returns a dataframe of the commment table. It has the timestamp, comment body and sentiment scores.
     Records without a """
-    df = pd.read_csv(COMMENTS_DATA)
-    df.datetime = pd.to_datetime(df.created_utc) #pick certain columns
+    df = pd.read_csv("wallstreetbets-posts-and-comments-for-august-2021-comments.csv")
+    df['datetime'] = pd.to_datetime(df.created_utc, unit='s').dt.tz_localize('UTC') #get timestamps
     df = df[['datetime', 'body', 'sentiment']] #pick certain columns
     df =  df[df.sentiment.notna()] #extract rows with existing sentiment scores
-    return df
-
-def get_market_sentiment(table : pd.DataFrame):#, start_time=None : datetime, end_time=None:datetime) ->  List[Tuple[datetime, int]]:
-    """Gets the market sentiment from the given start to end times, and returns a list
-    of (timestamp, sentiment) pairs."""
-    gme_mentions = table.body.str.contains("GME", case=False)
-    gamestop_mentions = table.body.str.contains("gamestop", case=False)
-    return table[gamestop_mentions | gme_mentions]
     
+    gme_mentions = df.body.str.contains("GME", case=False)
+    gamestop_mentions = df.body.str.contains("gamestop", case=False)
+    return df[gamestop_mentions | gme_mentions]
 
+def get_stock_data():
+    pass
 
 def graph_time_series(series1, series2)->None:
     """Graphs 2 """
     plt
     pass
 
-comments : pd.DataFrame = get_market_sentiment(get_comment_table()) 
+comments : pd.DataFrame = get_market_sentiment() 
 # comments.created_utc = pd.to_datetime(comments.created_utc)
 plt.scatter(comments.datetime, comments.sentiment)
 # plt.gca().xaxis.set_major_formatter(mdates.ConciseDateFormatter('%Y-%m-%d'))
