d4e9f1f7db36984d6fac80f075616d09f0f3ec92
ab2886 <ab2886>
2024-02-11 13:50:07 +0000

    Not sure if this is useful

--- ddl.sql
+++ ddl.sql
@@ -8,12 +8,46 @@ CREATE TABLE `stock_data` (
   `symbol` varchar(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+CREATE TABLE sns_data_sentiment (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  timestamp DATETIME,
+  user_id VARCHAR(255),
+  post_text TEXT,
+  sentiment VARCHAR(20),
+  positive_score FLOAT,
+  negative_score FLOAT,
+  neutral_score FLOAT,
+  mixed_score FLOAT
+); 
+
+
+-- TODO: remove your_database_name bit?
 CREATE TABLE your_database_name.Platform (
-	PlatformID      INTEGER auto_increment NOT NULL,
+	PlatformID      INT AUTO_INCREMENT NOT NULL,
 	PlatformName    varchar(20) NOT NULL,
-	CONSTRAINT Platform_PK PRIMARY KEY (PlatformID)
+	CONSTRAINT      PlatformID PRIMARY KEY (PlatformID)
+);
+
+CREATE TABLE your_database_name.User (
+	UserID      INT AUTO_INCREMENT NOT NULL,
+	PlatformID  INT NOT NULL,
+    Username    VARCHAR(40), -- TODO: Needs length adjustment
+	PRIMARY KEY (PlatformID),
+    FOREIGN KEY (ForeignID) REFERENCES your_database_name.Platform(PlatformID)
 )
-ENGINE=InnoDB
-DEFAULT CHARSET=utf8mb4
-COLLATE=utf8mb4_0900_ai_ci
-AUTO_INCREMENT=1;
+
+CREATE TABLE your_database_name.Api (
+	ApiID       INT AUTO_INCREMENT NOT NULL,
+    ApiName     VARCHAR(20), -- TODO: Needs length adjustment
+	ApiInfo     --TODO: What datatype is this?
+	ApiKey      VARCHAR(50) NOT NULL,
+	PRIMARY KEY (ApiID)
+)
+
+CREATE TABLE your_database_name.Request (
+	RequestID       INT AUTO_INCREMENT NOT NULL,
+    ApiID           INT NOT NULL, -- TODO: Needs changing?
+	ReqTimestamp    VARCHAR(50) NOT NULL, -- TODO: I think timestamp is a keyword? 
+	PRIMARY KEY (RequestID),
+    FOREIGN KEY (ApiID) REFERENCES your_database_name.Api(ApiID)
+)
