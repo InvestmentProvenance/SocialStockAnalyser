001b9fb0a7a7bf18a4c97e65d883214c3a2e51e9
Reuben Carolan <61869273+ReubenCa@users.noreply.github.com>
2024-02-19 12:49:04 +0000

    Reuben time series (#16)
    * Started Writing some time series analysis
    
    * Finished some basic time series correlation comparison
    
    * Eliminated some bugs

---
+++ time_series_correlation.py
@@ -0,0 +1,98 @@
+"""A module for analysing correlation with respect to a difference between two time series."""
+from database import data
+import pandas as pd
+import numpy as np
+from datetime import datetime
+import matplotlib.pyplot as plt 
+from alive_progress import alive_bar
+
+
+def time_series_compare(compare_func, 
+                         series_1_timestamps, series_1_values,
+                        series_2_timestamps, series_2_values, 
+                        time_differences):
+    """Compares two time series using the given function and returns the result as a np array"""
+    #Maybe will work with other time types not tested yet
+    assert type(series_2_timestamps.values[0]) == type(series_1_timestamps.values[0]) == np.datetime64, "Time series wrong type"
+    results = np.zeros(len(time_differences))
+    with alive_bar(len(time_differences)) as bar:
+        for i, time_diff in enumerate(time_differences):
+            results[i] = compare_func(
+                series_1_timestamps, series_1_values, 
+                series_2_timestamps + time_diff, series_2_values )
+            bar()
+    return results
+
+def correlation(stock_timestamps : np.array, stock_price : np.array, sns_timestamps : np.array, sns_sentiment : np.array) -> float :
+    """Calculates correlation between stock price and sentiment - Must be provided in sorted order"""
+    stock_timestamps, stock_price, sns_timestamps, sns_sentiment = np.array(stock_timestamps), np.array(stock_price), np.array(sns_timestamps), np.array(sns_sentiment)
+    assert len(stock_timestamps) == len(stock_price) 
+    assert len(sns_timestamps) == len(sns_sentiment)
+    assert all(stock_timestamps[i] <= stock_timestamps[i+1] for i in range(len(stock_timestamps)-1)), "Must be sorted"
+    assert all(sns_timestamps[i] <= sns_timestamps[i+1] for i in range(len(sns_timestamps)-1)), "Must be sorted"
+
+    correpsonding_sentiment = np.zeros(len(stock_timestamps))
+    #fill coreresponding sentiment with weighted average of nearest sns sentiments
+    for index, stock_time in enumerate(stock_timestamps):
+        #find the nearest sentiment time
+        nearest_sentiment_index = np.searchsorted(sns_timestamps, stock_time)
+        total = 0
+        totalweight = 0
+        count = 0
+        for i in range(-1, 2):
+            if i >= 0 and i < len(sns_sentiment):
+                weight = (abs(sns_timestamps[nearest_sentiment_index + i] - stock_time).seconds) +1
+                total += sns_sentiment[nearest_sentiment_index + i] * weight
+                totalweight += weight
+                count += 1
+
+        correpsonding_sentiment[index] = total/weight
+    return np.correlate(stock_price, correpsonding_sentiment)
+    
+
+#For testing - delete later
+def get_market_sentiment():
+    """Returns a dataframe of the commment table. It has the timestamp, comment body and sentiment scores.
+    Records without a """
+    df = pd.read_csv("wallstreetbets-posts-and-comments-for-august-2021-comments 1.csv")
+    df['datetime'] = pd.to_datetime(df.created_utc, unit='s').dt.tz_localize('UTC') #get timestamps
+    df = df[['datetime', 'body', 'sentiment']] #pick certain columns
+    df =  df[df.sentiment.notna()] #extract rows with existing sentiment scores
+    
+    gme_mentions = df.body.str.contains("GME", case=False)
+    gamestop_mentions = df.body.str.contains("gamestop", case=False)
+    return df[gamestop_mentions | gme_mentions]
+
+
+#Example usage
+if __name__ == "__main__":
+    
+    sns_data = get_market_sentiment()
+    sns_data['timestamp'] = pd.to_datetime(sns_data['datetime']) # TZ aware
+    lower_ts = min(sns_data['timestamp'])
+    upper_ts = max(sns_data['timestamp'])
+    stock_data = data.get_data("GME", lower_ts, upper_ts)
+
+    
+    
+    stock_data['timestamp'] = pd.to_datetime(stock_data['timestamp'], utc = True) # Not TZ aware, I guess I'm assuming UTC
+    sns_timestamps =np.flip( sns_data['timestamp'])
+    sns_sentiment = np.flip( sns_data['sentiment'])
+    
+    print("Initial Correlation:")
+    print(correlation(stock_data['timestamp'], stock_data['close'], sns_timestamps, sns_sentiment))
+
+    time_diffs_seconds = list(range(-4000, 4000, 20))
+    time_diffs = [pd.Timedelta(seconds=i) for i in time_diffs_seconds]
+    print("Rolling Correlations:")
+    rolling_correlations = time_series_compare(correlation,  
+                              stock_data['timestamp'],                             
+                            stock_data['close'],
+                                sns_timestamps,
+                                 sns_sentiment,
+                              time_diffs)
+    print(rolling_correlations)
+    plt.plot(time_diffs_seconds, rolling_correlations)
+    plt.show()
+    plt.savefig("correlation.png")
+    
