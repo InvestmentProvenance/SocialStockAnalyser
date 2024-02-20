078c37ca7d0ba0c566c612dd2f16a6b975877287
Reuben Carolan <reuben.carolan@gmail.com>
2024-02-20 17:49:40 +0000

    Tidy up

--- time_series_correlation.py
+++ time_series_correlation.py
@@ -1,14 +1,17 @@
 """A module for analysing correlation with respect to a difference between two time series."""
-from database import data
-import pandas as pd
-import numpy as np
-from datetime import datetime
-import matplotlib.pyplot as plt 
-import tqdm
 import multiprocessing
 from functools import partial
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import tqdm
+from database import data
 
-def __run_individual_correlation(compare_func, series1 : pd.Series, series2 : pd.Series, time_diff : pd.Timedelta):
+
+def __run_individual_correlation(compare_func,
+                                series1 : pd.Series,
+                                series2 : pd.Series,
+                                time_diff : pd.Timedelta):
     """Runs compare function once - used for multiprocessing"""
     s2 = series2.copy()
     s2.index = s2.index + time_diff
@@ -22,7 +25,11 @@ def time_series_compare(compare_func,
     #Maybe will work with other time types not tested yet
     series1.sort_index(inplace=True)
     series2.sort_index(inplace=True)
-    partial_individual_correlation = partial(__run_individual_correlation, compare_func, series1, series2)
+    partial_individual_correlation = partial(
+        __run_individual_correlation,
+        compare_func,
+        series1,
+        series2)
     with multiprocessing.Pool() as pool:
         results = list(tqdm.tqdm(
         pool.imap(partial_individual_correlation, time_differences)
@@ -35,14 +42,16 @@ def time_series_compare(compare_func,
 def correlation(stock_series: pd.Series, sns_series : pd.Series) -> float :
     """Calculates correlation between stock price and sentiment - Uses stock closing priceb
     Series must have timestamps in ascending order"""
-    
+
     corresponding_sentiment = [None] * (len(stock_series)-1)
     #fill coreresponding sentiment with weighted average of nearest sns sentiments
     for i, stock_timestamp in enumerate(stock_series.index):
         if i == len(stock_series)-1:
             continue
-        relevantsentiments = sns_series[pd.to_datetime(stock_timestamp):pd.to_datetime(stock_series.index[i+1])]
-        if (len(relevantsentiments) != 0):
+        relevantsentiments = sns_series[
+            pd.to_datetime(stock_timestamp):
+            pd.to_datetime(stock_series.index[i+1])]
+        if len(relevantsentiments) != 0:
             corresponding_sentiment[i] = np.mean(relevantsentiments)
         elif i != 0:
             corresponding_sentiment[i] = corresponding_sentiment[i-1]
@@ -66,13 +75,15 @@ def correlation(stock_series: pd.Series, sns_series : pd.Series) -> float :
 
 #For testing - delete later
 def get_market_sentiment():
-    """Returns a dataframe of the commment table. It has the timestamp, comment body and sentiment scores.
+    """Returns a dataframe of the commment table. It has the timestamp, 
+    comment body and sentiment scores.
     Records without a """
     df = pd.read_csv("wallstreetbets-posts-and-comments-for-august-2021-comments 1.csv")
-    df['datetime'] = pd.to_datetime(df.created_utc, unit='s').dt.tz_localize('UTC') #get timestamps
+    #get timestamps
+    df['datetime'] = pd.to_datetime(df.created_utc, unit='s').dt.tz_localize('UTC')
     df = df[['datetime', 'body', 'sentiment']] #pick certain columns
     df =  df[df.sentiment.notna()] #extract rows with existing sentiment scores
-    
+
     gme_mentions = df.body.str.contains("GME", case=False)
     gamestop_mentions = df.body.str.contains("gamestop", case=False)
     return df[gamestop_mentions | gme_mentions]
@@ -88,18 +99,10 @@ if __name__ == "__main__":
     lower_ts = min(sns_data['timestamp'])
     upper_ts = max(sns_data['timestamp'])
     stock_data = data.get_data("GME", lower_ts, upper_ts)
-
-    stock_data['timestamp'] = pd.to_datetime(stock_data['timestamp'], utc = True) # Not TZ aware, I guess I'm assuming UTC
-    #sns_timestamps =np.flip( sns_data['timestamp'])
-    #sns_sentiment = np.flip( sns_data['sentiment'])
-
+    # Not TZ aware, I guess I'm assuming UTC
+    stock_data['timestamp'] = pd.to_datetime(stock_data['timestamp'], utc = True)
     stock_series = stock_data.set_index('timestamp').close
     sns_series = sns_data.set_index('timestamp').sentiment
-
-
-    #print("Initial Correlation:")
-    #print(correlation(stock_series, sns_series))
-
 
     print("Rolling Correlations:")
 
