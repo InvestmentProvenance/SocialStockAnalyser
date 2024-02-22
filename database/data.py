780bb651ccd96fb0a372d8ce019b8a87b3df5c7a
Reuben Carolan <reuben.carolan@gmail.com>
2024-02-22 12:42:58 +0000

    Set stuff up

--- database/data.py
+++ database/data.py
@@ -19,3 +19,30 @@ def upload_data(csv_data:pd.DataFrame):
       csv_data is a pandas dataframe"""
     data_tuples = [tuple(x) for x in csv_data.to_records(index=False)]
     db_stock.upload_stock(raw_data=data_tuples)
+
+
+#Reuben's Job:
+#TODO: read from database instead of file
+def get_sns_data(ticker:str, start_time:datetime, end_time:datetime) -> pd.DataFrame :
+    """NOT THE ACTUAL METHOD, JUST A PALCEHOLDER FOR BRIJ TO TEST"""
+    df = pd.read_csv("wallstreetbets-posts-and-comments-for-august-2021-comments.csv")
+    df['datetime'] = pd.to_datetime(df.created_utc, unit='s').dt.tz_localize('UTC') #get timestamps
+    df.set_index('datetime', inplace=True)
+    df = df[['body', 'sentiment']] #pick certain columns
+    df =  df[df.sentiment.notna()] #extract rows with existing sentiment scores
+    
+    gme_mentions = df.body.str.contains("GME", case=False)
+    gamestop_mentions = df.body.str.contains("gamestop", case=False)
+    return df[gamestop_mentions | gme_mentions]
+
+
+
+#Brij's Job:
+#Series should be indexed by datetimes
+def price_volume(ticker:str, start_time:datetime, end_time:datetime) -> pd.Series :
+    pass
+
+def chat_volume(ticker:str, start_time:datetime, end_time:datetime, intervals:pd.Timedelta) -> pd.Series :
+    pass
+
+#TODO: Add log normal - needs to return pandas series
