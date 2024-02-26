5e29a70e6071067f3452714580836fb7a56584d5
han-a <hak37@cam.ac.uk>
2024-02-26 12:26:26 +0000

    added plot two series

---
+++ database/analysis.py
@@ -0,0 +1,19 @@
+def plotTwoSeries(red : pd.Series, blue : pd.Series) -> None:
+    """Plots both the given series. Both series need to have an datetime index"""
+    fig, red_ax = plt.subplots()
+    blue_ax = red_ax.twinx()
+
+    plt.ticklabel_format(style='plain')
+    red_plot, = red_ax.plot(red, color='red', label=red.name)
+    blue_plot, = blue_ax.plot(blue, color='blue', label=blue.name)
+
+    red_ax.xlabel = 'Time'
+    red_ax.yaxis.set_major_formatter(ScalarFormatter(useMathText=True)) #TODO: Add a y-axis limit at 0?
+    blue_ax.yaxis.set_major_formatter(ScalarFormatter(useMathText=True))
+    red_ax.set(ylabel=red.name)
+    blue_ax.set(ylabel=blue.name)
+
+    fig.autofmt_xdate(bottom=0.2, rotation=-30, ha='left')
+    plt.title(f"{red.name} vs {blue.name}")
+    plt.legend(handles=[red_plot, blue_plot])
+    plt.show()
