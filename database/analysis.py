796d7c8cc5c35432c02f82de68a287ef44d0b4d6
ab2886 <ab2886>
2024-02-26 13:07:15 +0000

    Parkinson variance added

--- database/analysis.py
+++ database/analysis.py
@@ -21,6 +21,14 @@ def stock_variance(ticker:str, start_time:datetime, end_time:datetime) -> pd.Ser
     df['squared_diff'] = (df['open'] - df['mean_price'])**2 + (df['close'] - df['mean_price'])**2
     return df['squared_diff']
 
+def stock_log_normal_parkinson_variance(ticker:str, start_time:datetime, end_time:datetime) -> pd.Series:
+    """Calculate the variance of the rate of return using Parkinson's Extreme Value Method."""
+    # Applying Parkinson's formula to calculate the variance
+    df = data.get_data(ticker, start_time, end_time)
+    df['parkinson_variance'] = (1 / (4 * np.log(2))) * ((np.log(df['high'] / df['low'])) ** 2)
+    # Calculate the average variance across all rows to represent the overall period's variance
+    return df['parkinson_variance']
+
 def plotTwoSeries(red : pd.Series, blue : pd.Series) -> None:
     """Plots both the given series. Both series need to have an datetime index"""
     fig, red_ax = plt.subplots()
@@ -41,4 +49,4 @@ def plotTwoSeries(red : pd.Series, blue : pd.Series) -> None:
     plt.legend(handles=[red_plot, blue_plot])
     plt.show()
 
-print(stock_variance("GME", datetime(2021, 1, 1), datetime(2021, 1, 30)))
+print(stock_log_normal_parkinson_variance("GME", datetime(2021, 1, 1), datetime(2021, 1, 30)))
