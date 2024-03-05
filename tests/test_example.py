6c2e39c923b3d5bbfde87c12c9e53915fe275a76
ab2886 <ab2886>
2024-03-05 12:11:15 +0000

    Successfully done upwards imports: PyLint is satisfied and it runs. Empty `__init__.py` files were added to all directories touched by the script. In order to run the script, you need to use the run configuration "Run current submodule" or "Run main". Under the hood, `python -m SocialStockAnalyser.tests.test_example` is run, in the `/workspaces/` dir.

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,7 +1,14 @@
 """Example of how to write tests"""
-
+from ..database import db_stock
 #test file must start with 'test_'
 
 def test_answer():
     """Example test"""
     assert True
+
+
+def test_list():
+    print(db_stock.comment_tickers())
+
+if __name__ == "__main__":
+    test_list()
