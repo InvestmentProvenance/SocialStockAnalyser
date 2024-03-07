bf5f6eed113354a0ca01589e1f7680e064c8513c
ab2886 <ab2886>
2024-03-07 08:46:12 +0000

    Refactor

--- frontend_dev/spike_chart.py
+++ frontend_dev/spike_chart.py
@@ -7,32 +7,20 @@ from datetime import datetime
 from ..database import data
 
 # Generate a random signal
-opens = data.get_sns_data("NIO", datetime(2022,1,3), datetime(2022,3,4)).sentiment
+# opens = data.get_sns_data("NIO", datetime(2022,1,3), datetime(2022,3,4)).sentiment
+opens = data.get_data("NIO", datetime(2022,1,3), datetime(2022,3,4)).open
 
 # Use the 'shapes' attribute from the layout to draw the vertical lines
 print("Lines start")
 
 data = go.Scattergl(x=opens.index, y=opens.values)
-fig = go.Figure(data)
 
 
 lines = []
 for time, open in opens.items():
     lines.append(
-        # {'type':'line', 'xref':'x', 'yref':'y', 'x0':time, 'y0':0, 'x1':time, 'y1':open, 'line':{}}
-        dict(
-            type='line',
-            # xref='x',
-            # yref='y',
-            x0=time,
-            y0=0,
-            x1=time,
-            y1=open,
-            line=dict(
-                color='grey',
-                width=0.5
-            )
-        )
+        {'type':'line', 'xref':'x', 'yref':'y', 'x0':time, 'y0':0,
+         'x1':time, 'y1':open, 'line':{'color':'grey', 'width':0.5}}
     )
 print("Lines done")
 
